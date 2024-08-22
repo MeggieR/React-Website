6a19d5aa8b5331e893f76bc5c403f7148519dfcf
MeggieR <mrotrigs@gmail.com>
2024-08-22 17:51:11 +0300

    Navbar, Herosection and Home section

--- react-website-yt-1/src/components/Navbar.js
+++ react-website-yt-1/src/components/Navbar.js
@@ -1,43 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
-
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-    return (
-      <>
-        <nav className="navbar">
-          <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-              TRVL <i className="fab fa-typo3" />
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-            </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}> 
-                <li to='/' className="nav-links" onClick={closeMobileMenu}>
-                    Home
-                </li>
-                <li to='/services' className="nav-links" onClick={closeMobileMenu}>
-                    Services
-                </li>
-                <li to='/products' className="nav-links" onClick={closeMobileMenu}>
-                    Products
-                </li>
-                <li to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
-                    Sign-up
-                </li>
-            </ul>
-            {Button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-          </div>
-        </nav>
-      </>
-    );
-  }
-  
 
-export default Navbar
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        // Show button on initial load
+        showButton();
+
+        // Add the event listener
+        window.addEventListener('resize', showButton);
+
+        // Cleanup the event listener on component unmount
+        return () => window.removeEventListener('resize', showButton);
+    }, []); // Corrected dependency array
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        TRVL <i className="fab fa-typo3" />
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/products' className="nav-links" onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign-up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} {/* Check 'button' state */}
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
